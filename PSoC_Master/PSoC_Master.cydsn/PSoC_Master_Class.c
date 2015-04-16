fa0382dc5b336e1cc1a6a98b9943acde57a82004
Kristian Thomsen <kroelle.thomsen@gmail.com>
2015-04-16 16:33:08 +0200

    Merged UART branch into Master
    .. only these files, though :)

--- PSoC_Master/PSoC_Master.cydsn/PSoC_Master_Class.c
+++ PSoC_Master/PSoC_Master.cydsn/PSoC_Master_Class.c
@@ -15,6 +15,9 @@
 #include "I2C_Class.h"
 #include "UART_Class.h"
 
+//Definitions
+#define CONVERT_TO_ASCII 48
+
 // Private data members
 int32 tempTemp;
 int32 tempHum;
@@ -22,11 +25,108 @@ int16 tempSoilHum[6];
 int32 tempLight;
 
 // Private prototypes
+typedef enum {IDLE, ADJW, ADJH, ADJV, ADJI} state;
+volatile state theState = IDLE;
+CY_ISR_PROTO(UART_ISR);
 
 
+//Global variables
+volatile int8 irrigationIndex = 0;
+int32 tempTemp = 0;
+int32 tempHum = 0;
+int16 tempSoilHum[6] = {0};
+int32 tempLight = 0;
 
 void initPSoC_Master(void){
+    UART_ISR_StartEx(UART_ISR);
+}
 
+// UART ISR
+CY_ISR(UART_ISR){
+    uint8 buff;
+    buff = dkRequest();
+    
+    if(theState == IDLE){
+        switch (buff){
+            case 'T':{ //RequestTemp
+                respondTemp(getTemp_DSP());		
+                break;
+            }
+            case 'L':{ //RequestLight
+                respondLight(getLight_DSP());
+                break;
+            }
+            case 'A':{ //RequestAirhum
+                respondHum(getHum_DSP());
+                break;
+            }        
+            case 'H':{ //TurnHeatOn
+                // 0x7 is the maximum value.
+                respondHeat(adjustHeat(0x7), 'H');
+                break;
+            }
+            case 'K':{ //TurnHeatOff
+                // 0x0 is the minimum value.
+                respondHeat(adjustHeat(0x0), 'K');
+                break;
+            }
+            case 'W':{ //AdjustWindow
+                theState = ADJW;
+                break;
+            }
+            case 'V':{ //Ventilation
+                theState = ADJV;
+                break;
+            }
+            case 'F':{ //Watering
+                theState = ADJI;
+                break;
+            }
+            /*case 'S':{ //
+                respondSoilHum(); //TODO: Add stuff here
+                break;
+            }*/
+            default:{
+                // Do nothing - let the DevKit8000 timeout
+                break;
+            }
+        }
+    }
+    else if(theState == ADJW){
+        if(buff-CONVERT_TO_ASCII == 1){
+            respondWin(adjustWindow(0xFF));
+        }
+        else{
+            respondWin(adjustWindow(0x00));
+        }
+        theState = IDLE;
+    }
+    else if(theState == ADJV){
+        if(buff-CONVERT_TO_ASCII == 1){
+            respondVent(adjustVentilation(0xFF));
+        }
+        else{
+            respondVent(adjustVentilation(0x00));
+        }
+        theState = IDLE;
+    }
+    else if(theState == ADJI){
+        if (!irrigationIndex){
+            irrigationIndex = buff;
+        }
+        else{
+            if (buff-CONVERT_TO_ASCII == 1){
+                respondIrri(adjustIrrigation(irrigationIndex-CONVERT_TO_ASCII-1, 0xFF));
+            }
+            else{
+                respondIrri(adjustIrrigation(irrigationIndex-CONVERT_TO_ASCII-1, 0x00));
+            }
+            irrigationIndex = 0;
+            theState = IDLE;
+        }
+    }
+    
+    UART_ClearRxInterruptSource(UART_GetRxInterruptSourceMasked());     // Clear interrupt flag
 }
 
 
